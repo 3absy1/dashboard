b515ec72d419e6aac0bb5312bfe6b882ebd689e0
3absy1 <yossfeabdaslm123@gmail.com>
2024-05-16 17:53:01 +0300

    aaa

--- Modules/ReferenceModule/resources/views/users-roles.blade.php
+++ Modules/ReferenceModule/resources/views/users-roles.blade.php
@@ -14,26 +14,8 @@
         <!-- ===============================================-->
         <title>Phoenix</title>
 
-        <!-- ===============================================-->
-    <!--    Favicons-->
-        <!-- ===============================================-->
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
-        <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
-        <script src="assets/js/config.js"></script>
+        @include('main.head-css')
 
-        <!-- ===============================================-->
-        <!--    Stylesheets-->
-        <!-- ===============================================-->
-
-        <link href="assets/css/theme.min.css" type="text/css" rel="stylesheet" id="style-default">
-
-        <link href="assets/css/datatable-bootstrap5.css" rel="stylesheet">
-        <link rel="stylesheet" href="assets/css/responsive-datatable-bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/buttons.bootstrap5.css">
-        <link href="assets/css/user.min.css" type="text/css" rel="stylesheet" id="user-style-default">
     </head>
     <body>
         <!-- ===============================================-->
